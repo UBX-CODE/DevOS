ede4d0f979abc289fcc9bedda7f2d9f8d1e0f5d8
UBX-CODE <ujjwalsharma1910@gmail.com>
2026-06-16 12:35:09 +0530

    ..

---
+++ DevOS-server/middleware/auth.middleware.ts
@@ -0,0 +1,34 @@
+import{Request, Response, NextFunction} from "express";
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (req: Request,res: Response,next:NextFunction) => {
+    try{
+        const authHeader = req.headers.authorization;
+
+        if(!authHeader){
+            return res.status(401).json({
+                success: false,
+                message: "No token provided"
+            });
+        }
+        const token = authHeader.split(" ")[1];
+
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message: "token missing"
+            })
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+
+        console.log(decoded);
+
+        next(); 
+    }catch(error){
+        return res.status(401).json({
+            success: false,
+            message: "Invalid token"
+        })
+    }
+};
--- DevOS-server/routes/auth.routes.ts
+++ DevOS-server/routes/auth.routes.ts
@@ -1,10 +1,17 @@
 import {Router} from "express";
 import {registerUser, loginUser} from "../controllers/auth.controller";
+import {authMiddleware} from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
+router.get("/me", authMiddleware, (req,res) => {
+    res.json({
+        success:true,
+        message: "protected route accessed"
+    })
+    
+})
 
 export default router;
