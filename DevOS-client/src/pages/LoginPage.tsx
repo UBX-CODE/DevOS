0df337ce1e39c002040c36ee1b87cb571e0cfd5f
UBX-CODE <ujjwalsharma1910@gmail.com>
2026-06-18 18:46:40 +0530

    feat: implement frontend authentication flow with context and protected routes

--- DevOS-client/src/pages/LoginPage.tsx
+++ DevOS-client/src/pages/LoginPage.tsx
@@ -1,9 +1,52 @@
-const LoginPage = () => {
+import { useState } from "react";
+import { loginUser } from "../services/auth.service";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const response = await loginUser({email,password,});
+
+      console.log("Response:", response);
+
+      login(response.token);
+
+      alert("Login Successful!");
+      navigate("/");
+    } catch (error: any) {
+      console.error(error);
+      alert(error?.response?.data?.message || "Login Failed");
+    }
+  };
+
   return (
-    <div>
-      Login
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1 className="text-3xl font-bold">Login</h1>
+      <input
+        type="email"
+        placeholder="Enter Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="border p-2 rounded w-80"
+      />
+      <input
+        type="password"
+        placeholder="Enter Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="border p-2 rounded w-80"
+      />
+
+      <button onClick={handleLogin} className="bg-black text-white px-4 py-2 rounded">Login</button>
     </div>
   );
-};
+}
 
 export default LoginPage;
---
+++ DevOS-client/src/services/auth.service.ts
@@ -0,0 +1,19 @@
+import api from "../api/axios";
+
+export interface LoginData{
+    email:string;
+    password: string;
+}
+export interface RegisterData{
+    name: string;
+    email: string;
+    password: string;
+}
+export const loginUser = async(data:LoginData) => {
+    const response = await api.post("/auth/login", data);
+    return response.data;
+};
+export const registerUser = async(data:RegisterData) => {
+    const response = await api.post("/auth/register", data);
+    return response.data;
+};
