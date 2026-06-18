0485b50f8e895e3a6074ad7a115d4e9b4a344d7b
UBX-CODE <ujjwalsharma1910@gmail.com>
2026-06-18 10:22:22 +0530

    feat: implement task CRUD operations with project ownership validation

--- DevOS-server/src/routes/task.routes.ts
+++ DevOS-server/src/routes/task.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import {createTask,} from "../controllers/task.controller";
+import {createTask,getProjectTask,getMyTasks,updateTask,deleteTask} from "../controllers/task.controller";
 import {authMiddleware,} from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.post("/",authMiddleware,createTask);
+router.get("/:projectId/tasks",authMiddleware,getProjectTask);
+router.get("/",authMiddleware,getMyTasks);
+router.patch("/:id",authMiddleware,updateTask);
+router.delete("/:id",authMiddleware,deleteTask);
+
 
 export default router;
