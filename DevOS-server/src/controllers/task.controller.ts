99da3da3ea1a1f9059da52c52e7ca6a9cd36cdc9
UBX-CODE <ujjwalsharma1910@gmail.com>
2026-06-18 10:06:50 +0530

    feat: implement task CRUD operations with project ownership checks

--- DevOS-server/src/controllers/task.controller.ts
+++ DevOS-server/src/controllers/task.controller.ts
@@ -33,3 +33,107 @@ export const createTask = async (req: Request,res: Response) => {
     });
   }
 };
+
+export const getProjectTask = async(res:Response , req:Request) => {
+  try{
+    const {projectId} = req.params;
+    const project = await Project.findOne({
+      _id: projectId,
+      userId: req.user?.userId,
+    });
+    if(!project){
+      return res.status(404).json({
+        success:false,
+        message: "Project not found",
+      });
+    }
+    const tasks = await Task.find({
+      projectId,
+    });
+    return res.status(200).json({
+      success:true,
+      count: tasks.length,
+      tasks,
+    });
+  }catch(error:any){
+  return res.status(500).json({
+    success: false,
+    message: error.message,
+  })
+  }
+};
+
+export const getMyTasks = async (res:Response, req:Request) => {
+  try{
+    const tasks = await Task.find({
+      userId: req.user?.userId,
+    });
+    return res.status(200).json({
+      success:true,
+      count: tasks.length,
+      tasks,
+    });
+  }catch(error:any){
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+};
+
+export const updateTask = async (req: Request,res: Response) => {
+  try {
+    const task = await Task.findOneAndUpdate({
+        _id: req.params.id,
+        userId: req.user?.userId,
+    },
+    req.body,
+    {
+      new: true,
+    });
+    if (!task) {
+      return res.status(404).json({
+        success: false,
+        message: "Task not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      task,
+    });
+
+  } catch (error: any) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const deleteTask = async (req: Request,res: Response) => {
+  try {
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      userId: req.user?.userId,
+    });
+
+    if (!task) {
+      return res.status(404).json({
+        success: false,
+        message: "Task not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message:"Task deleted successfully",
+    });
+
+  } catch (error: any) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+
+  }
+};
